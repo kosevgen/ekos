e36c63dbde118816464fe294ade327504c5db91a
kosevgen <kocmock43@gmail.com>
2017-11-09 23:53:29 +0200

    1 new project lesson_1

---
+++ lesson_1_classes/lesson_1_classes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_1_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("\t\t\t@This program for testing class@");
+
+            Console.ReadKey();
+
+        }
+    }
+}
