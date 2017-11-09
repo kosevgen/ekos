503d4e3b3d9b8d58f9917f7992e938615b6618ec
kosevgen <kocmock43@gmail.com>
2017-11-09 23:32:12 +0200

    0

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Class1
+    {
+
+        public static void Main() {
+
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Title = "First program!!!";
+
+     
+            Console.WriteLine("\tProgram abaut types\n");
+
+            int a = 256;
+            int b = 256000000;
+            double c = 3.141548778994553332;
+
+            Console.WriteLine(" int a = " + a);
+            Console.WriteLine("\n\n\n");
+            Console.WriteLine(" int b = " + b);
+            Console.WriteLine("\n\n\n");
+
+            Console.WriteLine(" double c = " + c);
+            Console.WriteLine("\n\n\n");
+            Console.WriteLine("\t THE END!!! ");
+            Console.ReadKey();
+
+
+        }
+
+}
+}
---
+++ lesson_0/Lesson_0/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Lesson_0
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.Clear();
+            Console.Title = "\t\t\tLesson 0";
+
+            Console.WriteLine("\tProgram abaut types\n");
+
+            int a = 100_000;
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
