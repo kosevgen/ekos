8c098a9e9fee6fef8b8da08e2777cdb71ad90fd1
kosevgen <kocmock43@gmail.com>
2017-12-05 18:53:49 +0200

    Signed-off-by: kosevgen <kocmock43@gmail.com>

---
+++ DBTest/DBTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DBTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SqlConnectionClass sqlConnection = new SqlConnectionClass();
+            SqlConnection sqlConnectionToMSSQL = new SqlConnection(sqlConnection.sqlConnectionString);
+            try
+            {
+
+                Console.Write("Establish connection to DB ... ");
+                sqlConnectionToMSSQL.Open();
+                Console.Write(" COMPLITE " + "\n");
+                Console.ReadKey();
+
+            }
+            catch(Exception ex) {
+
+                Console.Write("Error during establish connection to DB " + ex);
+                Console.ReadKey();
+
+            }
+
+            //do something
+
+            try
+            {
+
+                Console.Write("Closing connection to DB ... ");
+                sqlConnectionToMSSQL.Close();
+                Console.Write(" COMPLITE " + "\n");
+                Console.ReadKey();
+
+            }
+            catch (Exception ex) {
+
+                Console.Write("Error during closing connection to DB " + ex);
+                Console.ReadKey();
+
+            }
+            
+        }
+    }
+}
---
+++ DBTest/DBTest/SqlConnection.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.SqlClient;
+
+namespace DBTest
+{
+
+    public class SqlConnectionClass
+    {
+
+        static string datasource = "192.168.250.200";
+        static string database = "DBForTests";
+        static string username = "sa";
+        static string password = "ASDqwe!@#";
+        public string sqlConnectionString = @"Data Source=" + datasource + ";Initial Catalog="
+                    + database + ";Persist Security Info=True;User ID=" + username + ";Password=" + password;
+        
+               
+    }
+
+}
+       
+
+
